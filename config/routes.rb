14e55df10087800b40f18bff2368e2cb4c0d021d
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-02 18:47:07 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-    devise_for :users,
-    controllers: {
-      sessions: 'api/v1/sessions',
-      registrations: 'api/v1/registrations'
-    }
-    get 'user-data', to: 'users#show'
+    post "/signup", to: "users#create"
+    post "/login", to: "sessions#create"
 end
