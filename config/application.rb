c5035ebba9d501b3b625430edcc45d5f7bec2956
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-02 10:39:40 +0200

    Setup cookie store

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,11 @@ module CarRentalAppBackEnd
 
     # Configuration for the application, engines, and railties goes here.
     #
+    config.session_store :cookie_store, key: '_interslice_session'
+
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
+
+    config.middleware.use config.session_store, config.session_options
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
