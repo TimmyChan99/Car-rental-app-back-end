7eb0827c1ee980766bd80dd33510ae6390ab28da
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-02 10:39:07 +0200

    Authenticated users api

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::SessionsController < Devise::SessionsController
+    respond_to :json
+
+    private
+
+    def response_with(resource, _opts = {})
+        render json: {
+            message: "You have been successfully signed in",
+            user: current_user
+        }, status: :ok
+
+    end
+
+    def respond_to_on_destroy
+        log_out_success && return if current_user
+
+        log_out_failure
+    end
+
+    def log_out_success
+        render json: {message: "You have been successfully signed out"}, status: :ok
+    end
+
+    def log_out_failure
+        render json: {message: "Sign out failed"}, status: :unauthorized
+    end
+end
