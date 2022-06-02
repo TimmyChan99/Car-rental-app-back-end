25ab40d03333ad12864dd5f07de15f2469175af2
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-02 10:29:18 +0200

    Setup user model with devise & jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :jwt_authenticatable,
+          jwt_revocation_strategy: JwtDenylist
+
   has_many :reservations, dependent: :delete_all
   has_many :cars, dependent: :delete_all
 end
