bd74aed2c8b02b56ddccae88db721be3d8cdc7fa
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-02 19:46:50 +0200

    Fix linter errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,26 @@
 class SessionsController < ApplicationController
   def create
-      user = User.find_by_email(session_params[:email])
+    user = User.find_by_email(session_params[:email])
 
-      if user && user.authenticate(session_params[:password])
-          token = issue_token(user)
-          render json: {user: UserSerializer.new(user), jwt: token}
-      else
-          render json: {error: "Incorrect username or password."}
-      end
+    if user&.authenticate(session_params[:password])
+      token = issue_token(user)
+      render json: { user: UserSerializer.new(user), jwt: token }
+    else
+      render json: { error: 'Incorrect username or password.' }
+    end
   end
 
   def show
-      if logged_in?
-          render json: current_user
-      else
-          render json: {error: "User is not logged in/could not be found."}
-      end
+    if logged_in?
+      render json: current_user
+    else
+      render json: { error: 'User is not logged in/could not be found.' }
+    end
   end
 
   private
+
   def session_params
-      params.require(:session).permit(:email, :password)
+    params.require(:session).permit(:email, :password)
   end
 end
