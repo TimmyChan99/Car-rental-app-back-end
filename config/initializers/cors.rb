77f2b29bd078757a4ca5a2204c5c048a3a3732e8
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-06 09:42:41 +0200

    Configure client entry point

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,9 +5,10 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001/"
+    origins "*"
 
     resource "*",
       headers: :any,
