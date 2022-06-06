4261eddd79c7f54464e56d0c3857c1245fad23ee
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-06 09:41:38 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_02_144414) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_06_073627) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,12 +28,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_02_144414) do
   create_table "reservations", force: :cascade do |t|
     t.string "city"
     t.integer "duration"
-    t.date "date"
-    t.bigint "car_id", null: false
+    t.date "date_reserved"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["car_id"], name: "index_reservations_on_car_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
@@ -46,6 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_02_144414) do
   end
 
   add_foreign_key "cars", "users"
-  add_foreign_key "reservations", "cars"
   add_foreign_key "reservations", "users"
 end
