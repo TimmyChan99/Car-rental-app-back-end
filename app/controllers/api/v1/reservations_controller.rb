649ac7d73187c7158b25e799473bcd70043cb2ab
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-06 16:20:03 +0200

    Fix linter errors

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,23 +1,23 @@
 class Api::V1::ReservationsController < ApplicationController
-    def index
-        reservations = Reservation.find_by(params[:user_id])
+  def index
+    reservations = Reservation.find_by(params[:user_id])
 
-        render :json => reservations
+    render json: reservations
+  end
+
+  def create
+    reservation = Reservation.new(reservation_params)
+
+    if reservation.save
+      render json: reservation
+    else
+      render json: { errors: 'Reservation not created' }, status: :unprocessable_entity
     end
+  end
 
-    def create
-        reservation = Reservation.new(reservation_params)
+  private
 
-        if reservation.save
-            render :json => reservation
-        else
-            render :json => { errors: "Reservation not created" }, status: :unprocessable_entity
-        end
-    end
-
-    private
-
-    def reservation_params
-        params.require(:reservation).permit(:car_id, :user_id, :date_reserved, :duration, :city)
-    end
+  def reservation_params
+    params.require(:reservation).permit(:car_id, :user_id, :date_reserved, :duration, :city)
+  end
 end
