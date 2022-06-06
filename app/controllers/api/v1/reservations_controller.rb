02f9c31efb9c57bf16ca6f6575559b5cf701ab5f
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-06 16:00:33 +0200

    Created reservation endpoints

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::ReservationsController < ApplicationController
+    def index
+        reservations = Reservation.find_by(params[:user_id])
+
+        render :json => reservations
+    end
+
+    def create
+        reservation = Reservation.new(reservation_params)
+
+        if reservation.save
+            render :json => reservation
+        else
+            render :json => { errors: "Reservation not created" }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def reservation_params
+        params.require(:reservation).permit(:car_id, :user_id, :date_reserved, :duration, :city)
+    end
 end
