58ce93582c3017cffbf556cbfbef839c851b372d
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-06 16:03:09 +0200

    Created dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Car.create(name: "Toyota", reserved: false, description: "This is a toyota", user_id: 1)
+Car.create(name: "Honda", reserved: false, description: "This is a honda", user_id: 2)
+Car.create(name: "Ford", reserved: false, description: "This is a ford", user_id: 3)
