50b0b66b9e2c5752ac076503a8a31fb050a121e1
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-12 21:31:42 +0200

    Attached image to json data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Car.create(name: "Toyota", reserved: false, description: "This is a toyota", user_id: 1)
-Car.create(name: "Honda", reserved: false, description: "This is a honda", user_id: 2)
-Car.create(name: "Ford", reserved: false, description: "This is a ford", user_id: 3)
+Car.create(name: "Toyota", reserved: false, description: "This is a toyota", user_id: 1, price: 100, image: "https://www.tesla.com/roadster")
+Car.create(name: "Honda", reserved: false, description: "This is a honda", user_id: 1, price: 100, image: "https://www.tesla.com/roadster")
+Car.create(name: "Ford", reserved: false, description: "This is a ford", user_id: 1, price: 100, image: "https://www.tesla.com/roadster")
