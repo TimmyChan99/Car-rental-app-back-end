b955a334e063a7ebbb4849e03081819601d00690
TimmyChan99 <felemenoun@gmail.com>
2022-06-13 12:05:33 +0100

    Test api create car  request >> test the reponse body content

--- spec/requests/cars_spec.rb
+++ spec/requests/cars_spec.rb
@@ -8,58 +8,60 @@ RSpec.describe "Cars", type: :request do
     Car.create!(name: 'Jeep', description: 'black', price: 20, reserved: false, image: 'url', user_id: 1, id: 2)
   end
 
-  # describe "GET /cars" do
+  describe "GET /cars" do
 
-  #   it "Get list of cars: test the size" do
-  #     get '/api/v1/cars'
-  #     expect(json.size).to eq 2
-  #   end
+    it "Get list of cars: test the size" do
+      get '/api/v1/cars'
+      expect(json.size).to eq 2
+    end
 
-  #   it "Return the car name" do
-  #     get '/api/v1/cars'
-  #     car = json[0]
-  #     expect(car['name']).to eq 'BMW'
-  #   end
+    it "Return the car name" do
+      get '/api/v1/cars'
+      car = json[0]
+      expect(car['name']).to eq 'BMW'
+    end
 
-  #   it "Return the car description" do
-  #     get '/api/v1/cars'
-  #     car = json[1]
-  #     expect(car['description']).to eq 'black'
-  #   end
+    it "Return the car description" do
+      get '/api/v1/cars'
+      car = json[1]
+      expect(car['description']).to eq 'black'
+    end
 
-  #   it "Returns status code 200" do
-  #     get '/api/v1/cars'
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+    it "Returns status code 200" do
+      get '/api/v1/cars'
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-  # describe "GET /cars/:id" do
+  describe "GET /cars/:id" do
 
-  #   before do
-  #     get '/api/v1/cars/1'
-  #   end
+    before do
+      get '/api/v1/cars/1'
+    end
 
-  #   it "Get the car details: test the size" do
-  #     expect(json.size).to eq 9
-  #   end
+    it "Get the car details: test the size" do
+      expect(json.size).to eq 9
+    end
 
-  #   it "Return the car name" do
-  #     expect(json['name']).to eq 'BMW'
-  #   end
+    it "Return the car name" do
+      expect(json['name']).to eq 'BMW'
+    end
 
-  #   it "Return the car description" do
-  #     expect(json['description']).to eq 'blue'
-  #   end
+    it "Return the car description" do
+      expect(json['description']).to eq 'blue'
+    end
 
-  #   it "Return the car price" do
-  #     expect(json['price']).to eq "15.0"
-  #   end
+    it "Return the car price" do
+      expect(json['price']).to eq "15.0"
+    end
 
-  #   it "Return the car reserved" do
-  #     expect(json['reserved']).to eq true
-  #   end
-  # end
+    it "Return the car reserved" do
+      expect(json['reserved']).to eq true
+    end
+  end
+
   describe "POST /cars" do
+    
     context "Not authorized user" do
     before do
       post '/api/v1/cars/', params: {
@@ -94,19 +96,31 @@ RSpec.describe "Cars", type: :request do
           description: 'blue', 
           price: 15, 
           image: 'url',
-          user_id: 1
+          user_id: 1,
         }},
         headers: {
           Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyfQ.NSblJ-t1wVTJ4U9dOYKrL3ZMe5ksjT_xFWZlxv1jIsM'
         }
      end
 
-     it " Creat a new car test (not authorized)" do
+     it " Create a new car test (not authorized)" do
       expect(json.size).to eq 9
     end
 
     it "Returns status code 200" do
       expect(response).to have_http_status(:success)
+    end
+
+      it "Return the car name" do
+      expect(json['name']).to eq 'Renault'
+    end
+
+    it "Return the car description" do
+      expect(json['description']).to eq 'blue'
+    end
+
+    it "Return the car price" do
+      expect(json['price']).to eq "15.0"
     end
   end
   end
