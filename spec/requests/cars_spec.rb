947b16f8e7671a4d83b871f1fa45f10bafbcc5e4
TimmyChan99 <felemenoun@gmail.com>
2022-06-13 11:06:43 +0100

    Test api car details request >> test the reponse body size

--- spec/requests/cars_spec.rb
+++ spec/requests/cars_spec.rb
@@ -4,32 +4,40 @@ RSpec.describe "Cars", type: :request do
 
   before do
     User.create!(id: 1, name: 'Rida', email: 'example@mail.com', password: 'password')
-    Car.create!(name: 'BMW', description: 'blue', price: 15, reserved: true, image: 'url', user_id: 1)
-    Car.create!(name: 'Jeep', description: 'black', price: 20, reserved: false, image: 'url', user_id: 1)
+    Car.create!(name: 'BMW', description: 'blue', price: 15, reserved: true, image: 'url', user_id: 1, id:1)
+    Car.create!(name: 'Jeep', description: 'black', price: 20, reserved: false, image: 'url', user_id: 1, id: 2)
   end
 
-  describe "GET /cars" do
+  # describe "GET /cars" do
 
-    it "Get list of cars: test the size" do
-      get '/api/v1/cars'
-      expect(json.size).to eq 2
-    end
+  #   it "Get list of cars: test the size" do
+  #     get '/api/v1/cars'
+  #     expect(json.size).to eq 2
+  #   end
 
-    it "Return the car name" do
-      get '/api/v1/cars'
-      car = json[0]
-      expect(car['name']).to eq 'BMW'
-    end
+  #   it "Return the car name" do
+  #     get '/api/v1/cars'
+  #     car = json[0]
+  #     expect(car['name']).to eq 'BMW'
+  #   end
 
-    it "Return the car description" do
-      get '/api/v1/cars'
-      car = json[1]
-      expect(car['description']).to eq 'black'
-    end
+  #   it "Return the car description" do
+  #     get '/api/v1/cars'
+  #     car = json[1]
+  #     expect(car['description']).to eq 'black'
+  #   end
 
-    it "returns status code 200" do
-      get '/api/v1/cars'
-      expect(response).to have_http_status(:success)
+  #   it "returns status code 200" do
+  #     get '/api/v1/cars'
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  describe "GET /cars/:id" do
+
+    it "Get the car details: test the size" do
+      get '/api/v1/cars/1'
+      expect(json.size).to eq 9
     end
   end
 end
