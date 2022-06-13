8e0df674478ebdc843493dd76c6e3a422af94cf4
TimmyChan99 <felemenoun@gmail.com>
2022-06-13 10:50:12 +0100

    Test api cars list request >> test request status

--- spec/requests/cars_spec.rb
+++ spec/requests/cars_spec.rb
@@ -1,15 +1,15 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "Cars", type: :request do
-#   describe "GET /cars" do
-#     it "Get list of cars" do
-#       get '/api/v1/cars'
-#       expect(json.size).to eq 0
-#     end
+RSpec.describe "Cars", type: :request do
+  describe "GET /cars" do
+    # it "Get list of cars" do
+    #   get '/api/v1/cars'
+    #   expect(json.size).to eq 0
+    # end
 
-#     it "returns status code 200" do
-#       get '/api/v1/cars'
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-# end
+    it "returns status code 200" do
+      get '/api/v1/cars'
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
