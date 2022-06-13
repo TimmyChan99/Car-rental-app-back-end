793e91eecda9338fae5be942b0126fabf71faa2d
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2022-06-14 00:04:10 +0100

    Update car.rb

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,7 +2,7 @@ class Car < ApplicationRecord
   has_one :reservation
   belongs_to :user
 
-  has_many_attached :pictures
+  has_one_attached :pictures
 
   validates :name, :description, :price, presence: true
 end
