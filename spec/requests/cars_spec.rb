06ed5d8762a1fc2ecbd91115df442010ae5cf759
TimmyChan99 <felemenoun@gmail.com>
2022-06-13 10:48:45 +0100

    Setup ApiHelpers

---
+++ spec/requests/cars_spec.rb
@@ -0,0 +1,15 @@
+# require 'rails_helper'
+
+# RSpec.describe "Cars", type: :request do
+#   describe "GET /cars" do
+#     it "Get list of cars" do
+#       get '/api/v1/cars'
+#       expect(json.size).to eq 0
+#     end
+
+#     it "returns status code 200" do
+#       get '/api/v1/cars'
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+# end
