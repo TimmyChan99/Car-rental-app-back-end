4ac3730a130bfbeab7b0abd5ff39a364b94a76a3
lilyfrancis <ifnotlily@yahoo.com>
2022-06-16 19:36:13 +0100

    Update car_spec.rb

--- spec/requests/api/car_spec.rb
+++ spec/requests/api/car_spec.rb
@@ -1,6 +1,17 @@
 require 'swagger_helper'
 
 describe 'Cars Endpoints' do 
-
+	path '/api/v1/cars' do
+		get '/api/v1/cars' do
+			tags 'cars'
+			produces 'application/json'
+			
+			response '200', 'Get cars' do
+				User.create(name: 'user', email: 'user@dev.co', password: '123456', id: 1)
+				Car.create(name: 'BMW', description: 'blue', price: 15, reserved: true, user_id: 1)
+				run_test!
+			end
+		 end	
+	end
+  
 end
-
